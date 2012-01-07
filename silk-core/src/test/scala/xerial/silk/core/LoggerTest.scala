54e9bc44b6f67c9bf3db570088936667d7491135
Taro L. Saito <leo@xerial.org>
2012-01-07 17:45:42 +0900

    Developing logger for Scala

---
+++ silk-core/src/test/scala/xerial/silk/core/LoggerTest.scala
@@ -0,0 +1,15 @@
+package xerial.silk.core
+
+//--------------------------------------
+//
+// LoggerTest.scala
+// Since: 2012/01/07 15:38
+//
+//--------------------------------------
+
+/**
+ * @author leo
+ */
+class LoggerTest {
+
+}
