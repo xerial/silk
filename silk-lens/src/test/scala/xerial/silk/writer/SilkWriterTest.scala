7a1984876c07e24185adde82384dd7258363bf0a
Taro L. Saito <leo@xerial.org>
2012-01-17 14:33:02 +0900

    Object -> Silk text

---
+++ silk-lens/src/test/scala/xerial/silk/writer/SilkWriterTest.scala
@@ -0,0 +1,17 @@
+package xerial.silk.writer
+
+import xerial.silk.util.SilkSpec
+
+//--------------------------------------
+//
+// SilkWriterTest.scala
+// Since: 2012/01/17 13:52
+//
+//--------------------------------------
+
+/**
+ * @author leo
+ */
+class SilkWriterTest extendsSilkSpec {
+
+}
