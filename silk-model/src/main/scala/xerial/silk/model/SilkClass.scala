a0cb0ab60280f5b5c1a07b286af7e1a80626ef68
Taro L. Saito <leo@xerial.org>
2012-01-24 16:41:31 +0900

    Enhanced silk examples

---
+++ silk-model/src/main/scala/xerial/silk/model/SilkClass.scala
@@ -0,0 +1,15 @@
+package xerial.silk.model
+
+//--------------------------------------
+//
+// SilkClass.scala
+// Since: 2012/01/24 13:37
+//
+//--------------------------------------
+
+/**
+ * @author leo
+ */
+class SilkClass {
+
+}
---
+++ silk-model/src/main/scala/xerial/silk/model/SilkModel.scala
@@ -0,0 +1,15 @@
+package xerial.silk.model
+
+//--------------------------------------
+//
+// SilkModel.scala
+// Since: 2012/01/24 15:04
+//
+//--------------------------------------
+
+/**
+ * @author leo
+ */
+class SilkModel {
+
+}
