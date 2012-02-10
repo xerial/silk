28fb54d02e835e7a7180b8606383e546891edd2e
Taro L. Saito <leo@xerial.org>
2012-02-10 09:22:44 +0900

    Imported java code of Silk lexer

---
+++ silk-parser/src/main/java/xerial/silk/parser/SilkLexer.java
@@ -0,0 +1,130 @@
+/*--------------------------------------------------------------------------
+ *  Copyright 2011 Taro L. Saito
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *--------------------------------------------------------------------------*/
+//--------------------------------------
+// XerialJ
+//
+// SilkTokenScanner.java
+// Since: 2011/04/29 22:53:43
+//
+// $URL$
+// $Author$
+//--------------------------------------
+package xerial.silk.parser;
+
+import org.xerial.core.XerialException;
+import org.xerial.util.log.Logger;
+import xerial.silk.parser.token.SilkToken;
+import xerial.silk.util.BufferedScanner;
+import xerial.silk.util.ArrayDeque;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+
+/**
+ * Silk Token scanner
+ * 
+ * @author leo
+ * 
+ */
+public class SilkLexer
+{
+    public static enum State {
+        INIT, HERE_DOC, NODE_NAME, NODE_VALUE, ATTRIBUTE_NAME, ATTRIBUTE_VALUE
+    }
+
+    private static Logger         _logger         = Logger.getLogger(SilkLexer.class);
+
+    private int                   PREFETCH_SIZE   = 10;
+
+    private BufferedScanner scanner;
+    private State                 state           = State.INIT;
+    private ArrayDeque<SilkToken> tokenQueue      = new ArrayDeque<SilkToken>();
+    private long                  nProcessedLines = 0;
+
+    public SilkLexer(InputStream in) {
+        this.scanner = new BufferedScanner(in);
+    }
+
+    public SilkLexer(Reader in) {
+        this.scanner = new BufferedScanner(in);
+    }
+
+    public void close() throws IOException {
+        scanner.close();
+    }
+
+    /**
+     * Look ahead k tokens. If there is no token at k, return null
+     * 
+     * @param k
+     * @return
+     * @throws XerialException
+     */
+    public SilkToken LA(int k) throws XerialException {
+        if (k == 0)
+            throw new IllegalArgumentException("k must be larger than 0");
+        while (tokenQueue.size() < k && !hasNoMoreLine()) {
+            fill(PREFETCH_SIZE);
+        }
+
+        if (tokenQueue.size() < k)
+            return null;
+
+        return tokenQueue.peekFirst(k - 1);
+    }
+
+    private boolean hasNoMoreLine() throws XerialException {
+        return scanner.hasReachedEOF();
+    }
+
+    /**
+     * Read the next token
+     * 
+     * @return next token or null if no more token is available
+     * @throws XerialException
+     */
+    public SilkToken next() throws XerialException {
+        if (!tokenQueue.isEmpty())
+            return tokenQueue.pollFirst();
+
+        if (scanner.hasReachedEOF())
+            return null;
+
+        fill(PREFETCH_SIZE);
+        return next();
+    }
+
+    void fill(int prefetch_lines) throws XerialException {
+
+        // TODO line-based error recovery
+        try {
+            for (int i = 0; i < prefetch_lines && !hasNoMoreLine(); ++i) {
+                CharSequence line = scanner.nextLine();
+                SilkLineLexer silkLineLexer = new SilkLineLexer(line, state);
+                SilkTokensInLine tokensInLine = silkLineLexer.scan();
+                nProcessedLines++;
+                tokenQueue.addAll(tokensInLine.tokenList);
+                state = tokensInLine.nextLineState;
+            }
+        }
+        catch (IOException e) {
+            throw XerialException.convert(e);
+        }
+    }
+
+}
---
+++ silk-parser/src/main/java/xerial/silk/parser/token/SilkTokenType.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2012 Taro L. Saito
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//--------------------------------------
+// XerialJ
+//
+// SilkTokenType.java
+// Since: 2011/05/02 12:03:46
+//
+// $URL$
+// $Author$
+//--------------------------------------
+package xerial.silk.parser.token;
+
+import xerial.silk.util.UTF8String;
+
+import java.util.HashMap;
+
+public enum SilkTokenType {
+    Error,
+    EOF,
+    Unknown,
+    Indent,
+    Node("-"),
+    TabNode("-|"),
+    BlockNode("-*"),
+    SeqNode("->"),
+    HereDocSep("--"),
+    LineComment("#"),
+    NewLine,
+    QName,
+    Preamble("%"),
+    At("@"),
+    Colon(":"),
+    Comma(","),
+    Asterisk("*"),
+    Plus("+"),
+    Question("?"),
+    String,
+    Integer,
+    Real,
+    Separator("-"),
+    LParen("("),
+    RParen(")"),
+    LBracket("["),
+    RBracket("]"),
+    LSquare("<"),
+    RSquare(">"),
+    DataLine,
+    HereDoc,
+    BlankLine,
+    NodeValue
+
+    ;
+
+    private final String                           symbol;
+    private final CharSequence                     rawText;
+
+    private static HashMap<Integer, SilkTokenType> symbolTable = new HashMap<Integer, SilkTokenType>();
+
+    static {
+        for (SilkTokenType each : SilkTokenType.values()) {
+            if (each.symbol != null && each.symbol.length() == 1) {
+                int ch = each.symbol.charAt(0);
+                symbolTable.put(ch, each);
+            }
+        }
+    }
+
+    private SilkTokenType() {
+        this.symbol = "";
+        this.rawText = new UTF8String(this.symbol);
+    }
+
+    private SilkTokenType(String symbol) {
+        this.symbol = symbol;
+        this.rawText = new UTF8String(this.symbol);
+    }
+
+    public CharSequence toRawString() {
+        return rawText;
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return symbol;
+    }
+
+    public static SilkTokenType toTokenType(int symbol) {
+        return symbolTable.get(symbol);
+    }
+
+}
