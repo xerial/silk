549f3a1f2633c488900cf635dc8ccd3133a479ce
Taro L. Saito <leo@xerial.org>
2012-03-22 00:12:56 +0900

    Xmeans first draft

--- silk-core/src/main/scala/xerial/silk/util/mining/KMeans.scala
+++ silk-core/src/main/scala/xerial/silk/util/mining/KMeans.scala
@@ -21,6 +21,7 @@ import xerial.silk.util.Logging
 import collection.{GenTraversable, GenTraversableOnce, GenSeq}
 import collection.generic.FilterMonadic
 import collection.immutable.IndexedSeq
+import java.util.concurrent.ConcurrentSkipListSet
 
 //--------------------------------------
 //
@@ -37,11 +38,13 @@ class ClusteringInput[T](val point: Array[T], val metric: PointDistance[T]) {
     point.par.map(x => metric.getVector(x)).distinct
   }
 
+  def vectors = point.par.map(x => metric.getVector(x))
+
 }
 
 class Cluster[T](val input: ClusteringInput[T], val centroid: Array[Array[Double]], val clusterAssignment: Array[Int]) {
 
-  implicit def toVector(p:T) : Array[Double] = metric.getVector(p)
+  implicit def toVector(p: T): Array[Double] = metric.getVector(p)
 
   val N = input.N
   val point = input.point
@@ -52,15 +55,17 @@ class Cluster[T](val input: ClusteringInput[T], val centroid: Array[Array[Double
    */
   val K = centroid.length
 
-  private def pointIDsInCluster(clusterID:Int) : FilterMonadic[Int, IndexedSeq[Int]] = {
+  private def pointIDsInCluster(clusterID: Int): FilterMonadic[Int, IndexedSeq[Int]] = {
     (0 until input.N).withFilter(i => clusterAssignment(i) == clusterID)
   }
 
-  def pointsInCluster(clusterID: Int) : Seq[T] = {
+  def pointVectors = input.point.map(metric.getVector(_))
+
+  def pointsInCluster(clusterID: Int): Seq[T] = {
     pointIDsInCluster(clusterID).map(x => point(x))
   }
 
-  def pointVectorsInCluster(clusterID: Int) : Seq[Array[Double]] = {
+  def pointVectorsInCluster(clusterID: Int): Seq[Array[Double]] = {
     pointIDsInCluster(clusterID).map(x => metric.getVector(point(x)))
   }
 
@@ -91,7 +96,7 @@ class Cluster[T](val input: ClusteringInput[T], val centroid: Array[Array[Double
     min._1
   }
 
-  def reassignToClosestCentroid : Cluster[T] = {
+  def reassignToClosestCentroid: Cluster[T] = {
     val newClusterAssignment: Array[Int] = Array.fill(N)(-1)
     (0 until N).par.foreach {
       i =>
@@ -99,15 +104,14 @@ class Cluster[T](val input: ClusteringInput[T], val centroid: Array[Array[Double
     }
     new Cluster(input, centroid, newClusterAssignment)
   }
-  
-  
-  def newCluster(newCentroid: Array[Array[Double]], newClusterAssignment: Array[Int]) =
-    new Cluster(input, newCentroid, newClusterAssignment)
+
+  def updateCentroids(newCentroid: Array[Array[Double]]) =
+    new Cluster(input, newCentroid, clusterAssignment)
 
 }
 
 /**
- * Distance definition of data points for K-Means clustering
+ * Distance definition of data points for K-Means clustering. Use [[xerial.silk.util.mining.EuclidPointDistance]] to implement this trait.
  *
  * @author leo
  *
@@ -122,6 +126,13 @@ trait PointDistance[T] {
    * @return
    */
   def getVector(e: T): PointVector
+
+  /**
+   * The size of dimension;
+   *
+   * @return
+   */
+  def dimSize: Int
 
   /**
    * Return the distance between the given two points
@@ -141,7 +152,6 @@ trait PointDistance[T] {
    */
   def centerOfMass(points: GenTraversableOnce[PointVector]): PointVector
 
-  def squareError(points: GenTraversableOnce[PointVector], centor: PointVector): Double
 
   /**
    * Compute the lower bound of the points
@@ -166,50 +176,18 @@ trait PointDistance[T] {
    * @return
    */
   def move(point: PointVector, direction: PointVector): PointVector
-
-  /**
-   * The size of dimension;
-   *
-   * @return
-   */
-  def dimSize: Int
 }
 
+/**
+ * Standard Euclid point distance implementation
+ * @tparam T
+ */
 trait EuclidPointDistance[T] extends PointDistance[T] {
 
-  /**
-   * Get point vector of the element
-   * @param e
-   * @return
-   */
-  def getVector(e: T): PointVector
-
-  /**
-   * The size of dimension;
-   *
-   * @return
-   */
-  def dimSize: Int
-
-  /**
-   * Return the distance between the given two points
-   *
-   * @param a
-   * @param b
-   * @return |a-b|
-   */
   def distance(a: PointVector, b: PointVector): Double = {
     val sum = (0 until dimSize).par.map(col => Math.pow(a(col) - b(col), 2)).sum
     Math.sqrt(sum)
   }
-
-  /**
-   * Return the center of mass of the inputs
-   *
-   * @param points
-   *            list of input points
-   * @return |a+b+c+... | / N
-   */
   def centerOfMass(points: GenTraversableOnce[PointVector]): PointVector = {
     var n = 0
     val v = points.reduce((a, b) => {
@@ -218,36 +196,12 @@ trait EuclidPointDistance[T] extends PointDistance[T] {
     })
     v.map(x => x / n)
   }
-
-  def squareError(points: GenTraversableOnce[PointVector], centor: PointVector): Double = {
-    points.reduce((a, b) => a.zip(b).map(x => Math.pow(x._1 - x._2, 2))).sum
-  }
-
-  /**
-   * Compute the lower bound of the points
-   *
-   * @param points
-   * @return
-   */
   def lowerBound(points: GenTraversableOnce[PointVector]): PointVector = {
     points.reduce((a, b) => a.zip(b).map(x => Math.min(x._1, x._2)))
   }
-
-  /**
-   * Compute the upper bound of the points
-   *
-   * @param points
-   * @return
-   */
   def upperBound(points: GenTraversableOnce[PointVector]): PointVector = {
     points.reduce((a, b) => a.zip(b).map(x => Math.max(x._1, x._2)))
   }
-  /**
-   * Move the points to the specified direction to the amount of the given distance
-   *
-   * @param point
-   * @return
-   */
   def move(point: PointVector, direction: PointVector): PointVector = {
     point.zip(direction).map(x => x._1 + x._2)
   }
@@ -259,6 +213,11 @@ object KMeans {
     var maxIteration: Int = 300
   }
 
+  def apply[T](K: Int, point: Array[T], metric: PointDistance[T]): Cluster[T] = {
+    val kmeans = new KMeans(new ClusteringInput[T](point, metric))
+    kmeans.execute(K)
+  }
+
 }
 
 /**
@@ -267,7 +226,7 @@ object KMeans {
  * @author leo
  *
  */
-class KMeans[T](config: KMeans.Config) extends Logging {
+class KMeans[T](input: ClusteringInput[T], config: KMeans.Config = new KMeans.Config) extends Logging {
 
   type PointVector = Array[Double]
   private val random: Random = new Random(0)
@@ -281,70 +240,62 @@ class KMeans[T](config: KMeans.Config) extends Logging {
    * Randomly choose K-centroids from the input data set
    *
    * @param K
-   * @param input
    * @return
    */
-  protected def initCentroids(K: Int, input: ClusteringInput[T]): GenSeq[PointVector] = {
+  protected def initCentroids(K: Int): Array[PointVector] = {
     if (K > input.N)
-      throw new IllegalArgumentException("K(%d) must be smaller than N(%d)".format(K, input.N))
+      throw new IllegalArgumentException("K(=%d) must be smaller than N(%d)".format(K, input.N))
 
     val uniquePoints = input.uniqueVectors
     val UN = uniquePoints.length
 
+    if(UN <= K)
+      throw new IllegalArgumentException("K(=%d) must be larger than the number of unique points".format(K))
+
     def pickCentroid(centroids: List[PointVector], remaining: Int): List[PointVector] = {
       if (remaining == 0)
         centroids
-      val r = random.nextInt(UN)
-      val v = uniquePoints(r)
-      pickCentroid(v :: centroids, remaining - 1)
+      else {
+        val r = random.nextInt(UN)
+        val v = uniquePoints(r)
+        pickCentroid(v :: centroids, remaining - 1)
+      }
     }
 
-    pickCentroid(List(), K).toSeq
+    pickCentroid(List(), K).toArray
   }
 
   /**
    * @param K
    *            number of clusters
-   * @param input
-   *            input data points in the matrix format
    * @throws Exception
    */
-  def execute(K: Int, input:ClusteringInput[T]): Cluster[T] = {
-    execute(K, input, initCentroids(K, input))
+  def execute(K: Int): Cluster[T] = {
+    execute(K, initCentroids(K))
   }
 
   /**
    * @param K
    *            number of clusters
-   * @param input
-   *            input data points in the matrix format
    * @param centroid
    *            initial centroids
    * @throws Exception
    */
-  def execute(K: Int, input:ClusteringInput[T], centroid: GenSeq[PointVector]): Cluster[T] = {
+  def execute(K: Int, centroid: Array[PointVector]): Cluster[T] = {
 
-    /**
-     * Assign each point to the closest centroid
-     * @param cluster
-     * @return
-     */
-    def EStep(cluster:Cluster[T]) = cluster.reassignToClosestCentroid
     /**
      * Returns the list of new centroids by taking the center of mass of the points in the clusters
      *
      * @param cluster
      * @return
      */
-    def MStep(cluster: Cluster[T]): GenSeq[PointVector] = {
+    def MStep(cluster: Cluster[T]): Array[PointVector] = {
       val newCentroids = (0 until cluster.K).par.map {
         cid =>
           cluster.metric.centerOfMass(cluster.pointVectorsInCluster(cid))
       }
-      newCentroids
+      newCentroids.toArray
     }
-    
-    val maxIteration: Int = config.maxIteration
 
     /**
      * K-means clustering iteration
@@ -353,21 +304,22 @@ class KMeans[T](config: KMeans.Config) extends Logging {
      * @return
      */
     def iteration(i: Int, cluster: Cluster[T]): Cluster[T] = {
-
       debug("iteration: %d", i + 1)
-      if (i >= maxIteration)
+      if (i >= config.maxIteration)
         cluster
       else {
-        val newCluster = EStep(cluster)
+        // E-step: Assign each point to the closest centroid
+        val newCluster = cluster.reassignToClosestCentroid
         if (newCluster.averageOfDistance >= cluster.averageOfDistance) {
           cluster
         }
         else {
-          val centroids = MStep(newCluster)
-          if (hasDuplicate(centroids))
+          // M-step: update the centroids
+          val newCentroids = MStep(newCluster)
+          if (hasDuplicate(newCentroids))
             cluster
           else
-            iteration(i + 1, newCluster)
+            iteration(i + 1, newCluster.updateCentroids(newCentroids))
         }
       }
     }
--- silk-core/src/main/scala/xerial/silk/util/mining/XMeans.scala
+++ silk-core/src/main/scala/xerial/silk/util/mining/XMeans.scala
@@ -1,4 +1,25 @@
+/*
+ * Copyright 2012 Taro L. Saito
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package xerial.silk.util.mining
+
+import collection.GenSeq
+import xerial.silk.util.Logging
+import java.util.{ArrayList, Random}
+import annotation.tailrec
 
 //--------------------------------------
 //
@@ -7,9 +28,180 @@ package xerial.silk.util.mining
 //
 //--------------------------------------
 
+class XMeansCluster[T]
+(
+  val BIC: Double,
+  input: ClusteringInput[T],
+  centroid: Array[Array[Double]],
+  clusterAssignment: Array[Int])
+  extends Cluster[T](input, centroid, clusterAssignment) {
+  def this(BIC: Double, cluster: Cluster[T]) = this(BIC, cluster.input, cluster.centroid, cluster.clusterAssignment)
+
+}
+
+object XMeans {
+
+  /**
+   * Compute Bayesian Information Criteria (BIC) of the clusters
+   *
+   * @return BIC value
+   */
+  protected def computeBIC[T](cluster: Cluster[T]): Double = {
+    val R: Double = cluster.N
+    val K: Double = cluster.K
+    if (R <= cluster.K) {
+      return Double.MinValue
+    }
+    val sigmaSquare: Double = cluster.sumOfSquareError / (R - K)
+    val M: Int = cluster.metric.dimSize
+
+    def loop(k: Int, bic: Double): Double = {
+      if (k >= K)
+        bic
+      else {
+        val R_n = cluster.clusterAssignment.count(cid => cid == k)
+        val p1: Double = -((R_n / 2.0) * Math.log(2.0 * Math.Pi))
+        val p2: Double = -((R_n * M) / 2.0) * Math.log(sigmaSquare)
+        val p3: Double = -(R_n - K) / 2.0
+        val p4: Double = R_n * Math.log(R_n)
+        val p5: Double = -R_n * Math.log(R)
+        val likelihoodOfTheCluster: Double = p1 + p2 + p3 + p4 + p5
+
+        // This part accumulates penalties on the number of free parameters for each bic computation.
+        // This is not exactly the same with the original X-means paper by D. Pelleg, but
+        // without this accumulation, the penalty of adding new cluster becomes inadequately low.
+        val numberOfFreeParameters: Int = ((K - 1) + M * K + 1).asInstanceOf[Int]
+        val newBIC = bic + likelihoodOfTheCluster - (numberOfFreeParameters / 2.0) * Math.log(R_n)
+        loop(k + 1, newBIC)
+      }
+    }
+    // Compute the sum of the cluster likelihood
+    val bic = loop(0, 0.0)
+    if (bic.isNaN)
+      Double.MinValue
+    else
+      bic
+  }
+
+}
+
 /**
+ *
+ * Reference:
+ * X-means: Extending K-means with efficient estimation of the number of clusters. D. Pelleg et al.
+ *
  * @author leo
  */
-class XMeans {
+class XMeans[T](input: ClusteringInput[T]) extends Logging {
 
+  type PointVector = Array[Double]
+  private val rand: Random = new Random(0)
+
+  import XMeans._
+
+  /**
+   * @param maxK
+   *            maximum number of the cluster
+   * @return
+   * @throws Exception
+   */
+  def execute(maxK: Int): XMeansCluster[T] = {
+    val centroid = input.metric.centerOfMass(input.vectors)
+    val cluster: Cluster[T] = new Cluster[T](input, Array(centroid), Array.fill(input.N)(0))
+    val BIC = computeBIC(cluster)
+    iteration(new XMeansCluster(BIC, input, cluster.centroid, cluster.clusterAssignment), maxK)
+  }
+
+  protected def iteration(initCluster: XMeansCluster[T], maxK: Int): XMeansCluster[T] = {
+
+    def EStep(K: Int, cluster: XMeansCluster[T]): XMeansCluster[T] = {
+      val kmeans: KMeans[T] = new KMeans[T](cluster.input)
+      val kmeansCluster = kmeans.execute(K)
+      val BIC = computeBIC(kmeansCluster)
+      return new XMeansCluster(BIC, kmeansCluster)
+    }
+
+    def splitCentroids(cluster: XMeansCluster[T]): Array[PointVector] = {
+      @tailrec
+      def loop(k: Int, centroids: List[PointVector]): List[PointVector] = {
+        if (k >= cluster.K)
+          centroids
+        else {
+          val pointsInTheCluster = cluster.pointsInCluster(k).toArray
+          val singleCluster = new Cluster[T](new ClusteringInput(pointsInTheCluster, input.metric), Array(cluster.centroid(k)), Array.fill(pointsInTheCluster.length)(0))
+          val currentBIC = computeBIC(singleCluster)
+          // Split the cluster into two, then perform 2-means
+          val newCluster: Cluster[T] = splitCluster(singleCluster)
+          val newBIC = computeBIC(newCluster)
+          if (newCluster.K == 1 || newBIC < currentBIC) {
+            // Using current centroid as is
+            loop(k + 1, singleCluster.centroid(0) :: centroids)
+          }
+          else {
+            // Splitting the centroid into two has better BIC
+            loop(k + 1, newCluster.centroid.toList ::: centroids)
+          }
+        }
+      }
+      val newCentroids = loop(0, List())
+      newCentroids.distinct.toArray // remove duplicate points
+    }
+
+    def loop(current: XMeansCluster[T]): XMeansCluster[T] = {
+      debug("# of clusters = %d", current.K)
+      if (current.K >= maxK)
+        current
+      else {
+        val nextCentroids = splitCentroids(current)
+        val nextCluster = EStep(nextCentroids.length, current)
+        if (current.BIC >= nextCluster.BIC)
+          current
+        else
+          loop(nextCluster)
+      }
+    }
+
+    loop(initCluster)
+  }
+
+  /**
+   * Split the centroid of the input cluster (K=1) into two, then perform 2-means clustering
+   * @param cluster
+   * @return
+   */
+  protected def splitCluster(cluster: Cluster[T]): Cluster[T] = {
+    assert(cluster.K == 1)
+    val lowerBound: PointVector = input.metric.lowerBound(cluster.pointVectors)
+    val upperBound: PointVector = input.metric.upperBound(cluster.pointVectors)
+    val diameter: Double = {
+      val d = input.metric.distance(lowerBound, upperBound)
+      if (d.isInfinite)
+        Double.MaxValue / 2.0
+      else
+        d
+    }
+
+    def splitCentroid = {
+      val D = input.metric.dimSize
+
+      val direction: Array[Double] = Array.fill(D)(rand.nextDouble)
+      val sumSquare = Math.sqrt(direction.sum)
+      val factor: Double = diameter / sumSquare
+      val d = direction.map(_ * factor)
+      val r = direction.map(_ * -factor)
+
+      val c = cluster.centroid(0)
+      // Move two centroids to the opposite directions from the centroid c
+      Array(input.metric.move(c, d), input.metric.move(c, r))
+    }
+
+    if (diameter == 0.0) {
+      cluster
+    }
+    else {
+      val newCentroid = splitCentroid
+      val kmeans: KMeans[T] = new KMeans[T](cluster.input)
+      kmeans.execute(newCentroid.length, newCentroid)
+    }
+  }
 }
