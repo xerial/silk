cd2c9807514693f76fbead446a309103afced0a4
Taro L. Saito <leo@xerial.org>
2012-12-28 00:17:36 +0900

    Create a clone of closures by skipping constructor invocation that might contain null checks

--- silk/src/main/scala/xerial/silk/core/SilkSerializer.scala
+++ silk/src/main/scala/xerial/silk/core/SilkSerializer.scala
@@ -27,7 +27,8 @@ import java.io._
 import org.objectweb.asm.{Opcodes, ClassVisitor, MethodVisitor, ClassReader}
 import collection.mutable.{Set, Map}
 import xerial.core.log.Logger
-
+import java.lang.reflect.Constructor
+import xerial.lens.TypeUtil
 
 
 object LazyF0 {
@@ -73,8 +74,8 @@ class LazyF0[R](f: => R) {
  */
 object SilkSerializer extends Logger {
 
-  def checkClosure[R](f: LazyF0[R]) {
-    debug("check closure")
+  def cleanupClosure[R](f: LazyF0[R]) = {
+    debug("cleanup closure")
     val cl = f.functionClass
     debug("closure class: %s", cl)
 
@@ -83,6 +84,8 @@ object SilkSerializer extends Logger {
 
     debug("accessed fields: %s", finder.output)
 
+    val clone = instantiateClass(cl)
+    clone
   }
 
   /**
@@ -102,10 +105,10 @@ object SilkSerializer extends Logger {
   def serializeClosure[R](f: => R) = {
     val lf = LazyF0(f)
     debug("Serializing closure class %s", lf.functionClass)
-    checkClosure(lf)
+    val clean = cleanupClosure(lf)
     val b = new ByteArrayOutputStream()
     val o = new ObjectOutputStream(b)
-    o.writeObject(lf.functionInstance)
+    o.writeObject(clean)
     o.flush()
     o.close
     b.close
@@ -154,6 +157,16 @@ object SilkSerializer extends Logger {
     in.close()
     ret
   }
+
+
+  def instantiateClass(cl:Class[_]) : Any = {
+    val m = classOf[ObjectStreamClass].getDeclaredMethod("getSerializableConstructor", classOf[Class[_]])
+    m.setAccessible(true)
+    val constructor = m.invoke(null, cl).asInstanceOf[Constructor[_]]
+    constructor.newInstance()
+  }
+
+
 
   private def getClassReader(cl: Class[_]): ClassReader = {
     new ClassReader(cl.getResourceAsStream(
