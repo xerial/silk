367a0498a0cb4e4fe0d47135d6ec74780c871073
Taro L. Saito <leo@xerial.org>
2013-06-07 16:53:33 +0900

    Add executor implementation for testing

--- silk-core/src/main/scala/xerial/silk/mini/TaskManager.scala
+++ silk-core/src/main/scala/xerial/silk/mini/TaskManager.scala
@@ -6,8 +6,10 @@
 //--------------------------------------
 
 package xerial.silk.mini
+
+
 import scala.language.existentials
-import xerial.core.log.Logger
+import xerial.core.log.{LoggerFactory, Logger}
 
 /**
  * Interface for computing a result at remote machine
@@ -44,8 +46,8 @@ trait TaskManager {
    * @param task
    */
   def submitTask(task: Task[_])
-  def addListener(listener: TaskEventListener)
-  def removeListener(listener: TaskEventListener)
+  //def addListener(listener: TaskEventListener)
+  //def removeListener(listener: TaskEventListener)
 
   //def addNode(h:Host)
   //def lostNode(h:Host)
@@ -58,35 +60,117 @@ trait TaskEventListener {
   def onFailure(task: Task)
 }
 
-object SilkEnv {
 
-  private val defaultEnv = new SilkEnv
 
-  def getEnv : SilkEnv = defaultEnv
+abstract class CentralTaskManager extends TaskManager with TaskEventListener with Guard {
 
+  //private var listeners = Seq.empty[TaskEventListener]
+  val resourceManager : ResourceManager
+
+  import scala.collection.mutable
+
+  private val submitted = mutable.Queue[Task[_]]()
+  private val allocatedResource = mutable.Map[Task[_], WorkerResource]()
+
+  /**
+   * Submit a task and send the task to the SilkMaster.
+   * @param task
+   */
+  def submitTask(task: Task[_]) {
+    // TODO choose an appropriate amount of CPUs
+    val resource = resourceManager.acquireResource(1, -1)
+    allocatedResource += task -> resource
+    submitted += task
+
+    // Execute
+    val b = SilkMini.serializeObj(task)
+    runAt(resource.host, b)
+  }
+
+  def runAt(host:Host, serializedTask:Array[Byte])
+
+  def onCompletion(task: Task[_], result: Any) {
+
+  }
+
+  def onFailure(task: Task) {
+
+  }
+
+
+//  def addListener(listener: TaskEventListener) {
+//    listeners :+= listener
+//  }
+//
+//  def removeListener(listener: TaskEventListener) {
+//    listeners = listeners.filter(_ eq listener)
+//  }
 }
 
 
-trait Master {
 
 
-}
+trait Master extends Logger {
 
-class SilkEnv extends Logger {
+  val taskManager : TaskManager
 
-  val masterRef : Master = _
+  def receive(m:Message) {
+    m match {
+      case tu @ TaskStatusUpdate(executorID, taskID, status) =>
+        handleTaskStatusUpdate(tu)
+      case other =>
+        warn(s"unknown message: $other")
+    }
+  }
 
-  def updateStatus(executorID:Int, taskID:Int, status:TaskStatus) {
-    info(s"update status: [$taskID] $status")
-    // Send the status to the SilkMaster
+  def handleTaskStatusUpdate(tu:TaskStatusUpdate) {
+    tu match {
+      case TaskStarted() =>
+      case TaskFinished() =>
+      case TaskFailed(message) =>
+        error(s"[executor-${tu.executorID}] task ${tu.taskID} failed:${message}")
 
-
+    }
   }
 
 }
 
-sealed trait TaskStatus
+trait MasterRef {
+  def send(m:Message)
+}
 
+
+
+
+trait Message
+case class TaskStatusUpdate(executorID:Int, taskID:Int, status:TaskStatus) extends Message
+
+
+
+/**
+ * SilkEnv holds references to SilkMaster, DistributedCache, etc.
+ */
+trait SilkEnv extends Logger {
+
+  val master : MasterRef
+  val cache : DistributedCache
+
+  def updateStatus(executorID:Int, taskID:Int, status:TaskStatus) {
+    info(s"update status: [$taskID] $status")
+    // Send the status to the SilkMaster
+    master.send(TaskStatusUpdate(executorID, taskID, status))
+  }
+}
+
+
+
+
+
+
+/**
+ * Transaction record of task execution
+ */
+sealed trait TaskStatus
 case class TaskStarted() extends TaskStatus
 case class TaskFinished() extends TaskStatus
 case class TaskFailed(message:String) extends TaskStatus
@@ -99,9 +183,9 @@ case class TaskFailed(message:String) extends TaskStatus
  */
 class TaskExecutor(id:Int, host:Host) {
 
-  private val env = SilkEnv.getEnv
+  val env : SilkEnv = _
 
-  def execute(task:Task) = {
+  def execute(task:Task[_]) = {
 
     try {
       env.updateStatus(id, task.id, TaskStarted())
@@ -109,43 +193,65 @@ class TaskExecutor(id:Int, host:Host) {
       env.updateStatus(id, task.id, TaskFinished())
     }
     catch {
-      case e : Exception =>
+      case e : Throwable =>
         env.updateStatus(id, task.id, TaskFailed(e.getMessage))
     }
-
   }
 
 }
 
 
+// Representing an cluster environment for testing using cake pattern
 
+object TestSilkEnv {
 
+  val cache = new SimpleDistributedCache
 
-
-class CentralTaskManager extends TaskManager {
-
-  private var listeners = Seq.empty[TaskEventListener]
-  private val resourceManager = new ResourceManager
-
-
-  /**
-   * Submit a task and send the task to the SilkMaster.
-   * @param task
-   */
-  def submitTask(task: Task[_]) {
-
-
-
+  val master = new Master {
+    val taskManager = new CentralTaskManager with LocalRunner with TestResourceManager
   }
 
-
-  def addListener(listener: TaskEventListener) {
-    listeners :+= listener
-  }
-
-  def removeListener(listener: TaskEventListener) {
-    listeners = listeners.filter(_ eq listener)
+  val masterRef : MasterRef = new MasterRef {
+    def send(m: Message) {
+      master.receive(m)
+    }
   }
 }
 
+trait TestSilkEnv extends SilkEnv {
+  val env = new SilkEnv {
+    val master = TestSilkEnv.masterRef
+    val cache = TestSilkEnv.cache
+  }
+}
+
+trait TestResourceManager {
+  val resourceManager : ResourceManager = {
+    val r = new ResourceManager
+    r.addResource(WorkerResource(Host("h1"), 2, 1 * 1024 * 1024))
+    r.addResource(WorkerResource(Host("h2"), 2, 1 * 1024 * 1024))
+    r
+  }
+}
+
+trait LocalRunner { this: TaskManager =>
+
+  private val logger = LoggerFactory(classOf[LocalRunner])
+  private val executor = collection.mutable.Map[Host,TaskExecutor]()
+
+  private var executorCount = 0
+
+  def runAt(host:Host, serializedTask:Array[Byte]) = {
+    logger.debug(s"runAt $host")
+
+    val ec = executor.getOrElseUpdate(host, {
+      executorCount += 1
+      val newID = executorCount
+      new TaskExecutor(newID, host)
+    })
+
+    val task = SilkMini.deserializeObj[Task[_]](serializedTask)
+    ec.execute(task)
+  }
+}
 
