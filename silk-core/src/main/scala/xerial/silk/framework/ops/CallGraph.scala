801f17b79659e2bbe67c802f5d85cee36d3999db
Taro L. Saito <leo@xerial.org>
2013-08-02 18:17:56 +0900

    Attempt to infer local variable types from ru.Expr, but this doesn't work because type information is lost

--- silk-core/src/main/scala/xerial/silk/framework/ops/CallGraph.scala
+++ silk-core/src/main/scala/xerial/silk/framework/ops/CallGraph.scala
@@ -11,6 +11,7 @@ import java.util.UUID
 import xerial.silk.Silk
 import xerial.lens.TypeUtil
 import xerial.core.log.Logger
+import xerial.silk.util.MacroUtil
 
 object CallGraph extends Logger {
   private class CallGraphBuilder {
@@ -40,12 +41,12 @@ object CallGraph extends Logger {
   }
 
 
-  def apply[A](op:Silk[A]) = createCallGraph(op)
+  def apply[A](op: Silk[A]) = createCallGraph(op)
 
-  import scala.reflect.runtime.{universe=>ru}
+  import scala.reflect.runtime.{universe => ru}
   import ru._
 
-  def createCallGraph[A](op: Silk[A]) : CallGraph = {
+  def createCallGraph[A](op: Silk[A]): CallGraph = {
     val g = new CallGraphBuilder
 
     def loop(node: Silk[_]) {
@@ -57,34 +58,70 @@ object CallGraph extends Logger {
           g.addEdge(in, node)
         }
 
-        node match {
-          case MapOp(id, fc, in, f, fe) =>
-           val ft = fe.tree.freeTerms
-           debug(s"free terms of $node: $ft")
-          case _ =>
-        }
+
+//        node match {
+//          case MapOp(id, fc, in, f, fe) =>
+//            val ft = fe.tree.freeTerms
+//            val fvNameSet = ft.map(_.name.decoded).toSet
+//            fe.tree
+//            debug(s"fexpr: ${showRaw(fe)}")
+//            fe.staticType match {
+//              case t @TypeRef(prefix, symbol, List(from, to)) =>
+//                debug(s"f: ${from} => ${to}")
+//              case _ =>
+//            }
+//            debug(s"free terms of ${node.idPrefix}: $ft")
+//
+//            val b = Seq.newBuilder[(String, Type)]
+//            val tv = new Traverser {
+//              override def traverse(tree: ru.Tree) {
+//                def matchIdent(idt: Ident): ru.Tree = {
+//                  val name = idt.name.decoded
+//                  if (fvNameSet.contains(name)) {
+//                    val tt: ru.Tree = MacroUtil.toolbox.typeCheck(idt, silent = true)
+//                    b.+=((idt.name.decoded, tt.tpe))
+//                    tt
+//                  }
+//                  else
+//                    idt
+//                }
+//
+//                tree match {
+//                  case idt@Ident(term) =>
+//                    matchIdent(idt)
+//                  case v @ValDef(mod, name, tpt, rhs) =>
+//                    val tt: ru.Tree = MacroUtil.mirror.runtimeClass(tree)
+//                    debug(s"find $name, type = ${tt}")
+//                  case other => super.traverse(other)
+//                }
+//              }
+//            }
+//            tv.traverse(fe.tree)
+//            debug(s"traverse result: ${b.result.distinct}")
+//          case _ =>
+//        }
 
         // Do not traverse CallGraph by inputting a dummy data, because its cost might be prohibitive.
         // Instead create schedule graph dynamically
-//        node match {
-//          case mo @ MapOp(id, fc, in, f, fe) =>
-//            fe.staticType match {
-//              case t @ TypeRef(prefix, symbol, List(from, to)) =>
-//                if(isSilkType(mirror.runtimeClass(to))) {
-//                  // Run the function to obtain its result by using a dummy input
-//                  val inputCl = mirror.runtimeClass(from)
-//                  val z = zero(inputCl)
-//                  val nextExpr = mo.fwrap.apply(z).asInstanceOf[Silk[_]]
-//                  debug(s"next expr: $nextExpr")
-//                  // Replace the dummy input
-//                  val gsub = createCallGraph(nextExpr)
-//                  debug(gsub)
-//                }
-//              case other => warn(s"unknown type: ${other}")
-//            }
-//
-//          case other =>
-//        }
+        //        node match {
+        //          case mo @ MapOp(id, fc, in, f, fe) =>
+        //            fe.staticType match {
+        //              case t @ TypeRef(prefix, symbol, List(from, to)) =>
+        //                if(isSilkType(mirror.runtimeClass(to))) {
+        //                  // Run the function to obtain its result by using a dummy input
+        //                  val inputCl = mirror.runtimeClass(from)
+        //                  val z = zero(inputCl)
+        //                  val nextExpr = mo.fwrap.apply(z).asInstanceOf[Silk[_]]
+        //                  debug(s"next expr: $nextExpr")
+        //                  // Replace the dummy input
+        //                  val gsub = createCallGraph(nextExpr)
+        //                  debug(gsub)
+        //                }
+        //              case other => warn(s"unknown type: ${other}")
+        //            }
+        //
+        //          case other =>
+        //        }
 
       }
     }
@@ -92,10 +129,10 @@ object CallGraph extends Logger {
     g.result
   }
 
-  def isSilkType[A](cl:Class[A]) : Boolean = classOf[Silk[_]].isAssignableFrom(cl)
+  def isSilkType[A](cl: Class[A]): Boolean = classOf[Silk[_]].isAssignableFrom(cl)
   private[silk] val mirror = ru.runtimeMirror(Thread.currentThread.getContextClassLoader)
 
-  def zero[A](cl:Class[A]) = cl match {
+  def zero[A](cl: Class[A]) = cl match {
     case f if isSilkType(f) =>
       Silk.empty
     case _ => TypeUtil.zero(cl)
@@ -112,7 +149,7 @@ case class CallGraph(nodes: Seq[Silk[_]], edges: Map[UUID, Seq[UUID]]) {
   private val idToSilkTable = nodes.map(x => x.id -> x).toMap
   private val silkToIdTable = nodes.map(x => x -> x.id).toMap
 
-  private def idPrefix(uuid:UUID) = uuid.toString.substring(0, 8)
+  private def idPrefix(uuid: UUID) = uuid.toString.substring(0, 8)
 
   override def toString = {
     val s = new StringBuilder
@@ -127,17 +164,17 @@ case class CallGraph(nodes: Seq[Silk[_]], edges: Map[UUID, Seq[UUID]]) {
     s.toString
   }
 
-  def childrenOf(op:Silk[_]) : Seq[Silk[_]] = {
+  def childrenOf(op: Silk[_]): Seq[Silk[_]] = {
     val childIDs = edges.getOrElse(op.id, Seq.empty)
     childIDs.map(cid => idToSilkTable(cid))
   }
 
-  def descendantsOf(op:Silk[_]) : Set[Silk[_]] = {
+  def descendantsOf(op: Silk[_]): Set[Silk[_]] = {
     var seen = Set.empty[Silk[_]]
-    def loop(current:Silk[_]) {
-      if(seen.contains(current))
+    def loop(current: Silk[_]) {
+      if (seen.contains(current))
         seen += current
-      for(child <- childrenOf(current))
+      for (child <- childrenOf(current))
         loop(child)
     }
     loop(op)
