8414ca88df1fe271f459ecb752b43405a735c52e
Taro L. Saito <leo@xerial.org>
2013-08-20 01:29:40 +0900

    Request mapping to /(method name) in Root

---
+++ silk-webui/src/main/scala/xerial/silk/webui/app/Root.scala
@@ -0,0 +1,27 @@
+//--------------------------------------
+//
+// Root.scala
+// Since: 2013/08/20 1:18
+//
+//--------------------------------------
+
+package xerial.silk.webui.app
+
+import xerial.silk.webui.{path, WebAction}
+
+/**
+ * Methods in Root is mapped to http://(web UI address)/(method name)
+ *
+ * @author Taro L. Saito
+ */
+class Root extends WebAction {
+
+  @path("/index")
+  def index {
+    setContent(s"top page")
+    render
+  }
+
+
+
+}
