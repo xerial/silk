7dfcf9ef6ada4817f87bebd985393329bf5ff054
Taro L. Saito <leo@xerial.org>
2013-10-15 18:01:52 +0900

    Remove slf4j initialization

--- silk-weaver/src/multi-jvm/scala/xerial/silk/cluster/ClusterSpec.scala
+++ silk-weaver/src/multi-jvm/scala/xerial/silk/cluster/ClusterSpec.scala
@@ -77,8 +77,6 @@ trait CuratorBarrier {
 
 trait ClusterSpecBase extends SilkSpec with ProcessBarrier with CuratorBarrier {
 
-  xerial.silk.configureLog4j
-
   def processID = {
     val n = getClass.getSimpleName
     val p = "[0-9]".r
