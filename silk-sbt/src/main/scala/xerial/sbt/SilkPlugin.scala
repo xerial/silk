ca6707c850678ee34d068793645a64160aa985b6
Taro L. Saito <leo@xerial.org>
2013-11-14 11:08:10 +0900

    Enclose silk-sbt keys within SilkKeys object

--- silk-sbt/src/main/scala/xerial/sbt/SilkPlugin.scala
+++ silk-sbt/src/main/scala/xerial/sbt/SilkPlugin.scala
@@ -1,6 +1,6 @@
 //--------------------------------------
 //
-// Silk.scala
+// SilkPlugin.scala
 // Since: 2013/11/13 2:03 PM
 //
 //--------------------------------------
@@ -22,33 +22,38 @@ import sbt.File
  */
 object SilkPlugin extends sbt.Plugin {
 
-  val silkRun = InputKey[Unit]("silk-run", "Run a silk expression")
-  val silkFork = settingKey[Boolean]("fork JVM when running silk")
-  val silkVersion = settingKey[String]("silk version to use")
-  val silkJvmOpts = settingKey[Seq[String]]("JVM options to use when launching Silk")
+  object SilkKeys {
+    val silk = InputKey[Unit]("silk", "Run a silk command")
+    val silkRun = InputKey[Unit]("silk-run", "Run a silk expression")
+    val silkFork = settingKey[Boolean]("fork JVM when running silk")
+    val silkVersion = settingKey[String]("silk version to use")
+    val silkJvmOpts = settingKey[Seq[String]]("JVM options to use when launching Silk")
+  }
+
+  import SilkKeys._
 
   lazy val silkSettings = Seq[Def.Setting[_]](
     silkVersion := version.value,
     silkJvmOpts := Seq("-Xmx512m"),
     silkFork := false,
-    silkRun := {
+    silk := {
       val s = streams.value
       val logger = s.log
       val args : Seq[String] = spaceDelimited("<args>").parsed
-      logger.debug(s"silk run: args:[${args.mkString(", ")}]")
+      logger.debug(s"Run silk: args:[${args.mkString(", ")}]")
       val fullCp : Seq[File] = data((fullClasspath in Runtime).value)
 
-      val evalCmdLine = s"eval ${args.mkString(" ")}"
-      logger.info(s"Run $evalCmdLine")
+      val cmdLine = s"${args.mkString(" ")}"
+      logger.info(s"Run silk: $cmdLine")
 
       val doFork = silkFork.value
       if(doFork) {
         // Launch a JVM
         logger.debug(s"Fork JVM")
         logger.debug(s"class path:${fullCp}")
-        val cmdLine = s"-classpath ${Path.makeString(fullCp)} ${silkJvmOpts.value.mkString(" ")} xerial.silk.weaver.SilkMain $evalCmdLine"
-        logger.debug(s"command line: $cmdLine")
-        val proc = Shell.launchJava(cmdLine)
+        val javaCmdLine = s"-classpath ${Path.makeString(fullCp)} ${silkJvmOpts.value.mkString(" ")} xerial.silk.weaver.SilkMain $cmdLine"
+        logger.debug(s"command line: $javaCmdLine")
+        val proc = Shell.launchJava(javaCmdLine)
 
         val exitValue = proc.waitFor()
         logger.info(s"terminated ($exitValue)")
@@ -65,7 +70,7 @@ object SilkPlugin extends sbt.Plugin {
           sys.props.update("java.class.path", fullCp.mkString(File.pathSeparator))
           val mainClass = Class.forName("xerial.silk.weaver.SilkMain", true, cl)
           val mainMethod = mainClass.getDeclaredMethod("main", classOf[java.lang.String])
-           mainMethod.invoke(null, evalCmdLine)
+          mainMethod.invoke(null, cmdLine)
         }
         finally {
           Thread.currentThread.setContextClassLoader(prevCl)
