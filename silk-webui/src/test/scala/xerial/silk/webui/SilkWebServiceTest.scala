152b4abccfdfc9511bbe985150f2d9d1a9de4018
Taro L. Saito <leo@xerial.org>
2013-12-30 15:48:51 +0900

    Add SilkWebUI test

--- silk-webui/src/test/scala/xerial/silk/webui/SilkWebServiceTest.scala
+++ silk-webui/src/test/scala/xerial/silk/webui/SilkWebServiceTest.scala
@@ -9,38 +9,55 @@ package xerial.silk.webui
 
 import xerial.silk.util.SilkSpec
 import xerial.core.io.IOUtil
-import java.net.URL
+import java.net.{HttpURLConnection, URL}
+import org.scalatest.BeforeAndAfterAll
 
 /**
  * @author Taro L. Saito
  */
-class SilkWebServiceTest extends SilkSpec {
+class SilkWebServiceTest extends SilkSpec with BeforeAndAfterAll {
+
+  var si : SilkWebService = null
+
+  def get(path:String) : String = {
+    val url = new URL(s"http://localhost:${si.port}/$path")
+    val conn = url.openConnection().asInstanceOf[HttpURLConnection]
+    conn.setRequestMethod("GET")
+    val ret = conn.getResponseCode
+    ret match {
+      case HttpURLConnection.HTTP_OK =>
+        IOUtil.readFully(conn.getInputStream) { b =>
+          val s = new String(b)
+          debug(s"contents:\n$s")
+          s
+        }
+      case _ =>
+        fail(s"Web server returned code: $ret")
+    }
+  }
+
+  before {
+    si = new SilkWebService(IOUtil.randomPort)
+  }
+
+  after {
+    si.close
+  }
+
+
   "SilkWebService" should {
 
     "start jetty" in {
-      for(si <- SilkWebService(IOUtil.randomPort)) {
-        val addr = new URL(s"http://localhost:${si.port}/hello.txt")
-        IOUtil.readFully(addr.openStream) { b =>
-          val contents = new String(b)
-          info(s"contents: $contents")
-          contents should (include ("Hello Silk"))
-        }
-
-        val addr2 = new URL(s"http://localhost:${si.port}/task/list")
-        IOUtil.readFully(addr2.openStream) { b =>
-          val contents = new String(b)
-          contents should (include ("<body"))
-        }
-
-
-        val addr3 = new URL(s"http://localhost:${si.port}/")
-        IOUtil.readFully(addr3.openStream) { b =>
-          val contents = new String(b)
-          contents should (include ("<body"))
-        }
-
-      }
+      get("hello.txt") should (include ("Hello Silk"))
+      get("task/list") should (include ("<body"))
+      get("") should (include ("<body"))
     }
+
+    "display node list" taggedAs("nodelist") in {
+      get("node/list")
+    }
+
+
 
   }
 
